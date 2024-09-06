49414116fd46531cea23232573b1307391b65f21
Raine DeVries <362571390@GoTVDSB.ca>
2024-09-06 09:21:40 -0400

    Finished

--- Topic 1 Assignment/Program.cs
+++ Topic 1 Assignment/Program.cs
@@ -1,4 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//Raine DeVries
 Console.WriteLine("Hello, my name is Raine DeVries");
 Console.WriteLine("");
-Console.WriteLine("Comments are importante beacause");
+Console.WriteLine("Comments are importante beacause they can be used to explaine yourself, to show bugs, to show how it is used or to give credit to someone");
+Console.WriteLine("");
+Console.WriteLine("RRRR   DDDD");
+Console.WriteLine("R   R  D   D");
+Console.WriteLine("R   R  D   D");
+Console.WriteLine("RRRR   D   D");
+Console.WriteLine("R R    D   D");
+Console.WriteLine("R  R   D   D");
+Console.WriteLine("R   R  DDDD");
+Console.ReadLine();
+
+
