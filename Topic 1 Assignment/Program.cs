604d8fdf6ec096b5a4bcbaefd62ee0899c2b736f
Raine DeVries <362571390@GoTVDSB.ca>
2024-09-06 08:50:21 -0400

    Just getting started

--- Topic 1 Assignment/Program.cs
+++ Topic 1 Assignment/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, my name is Raine DeVries");
+Console.WriteLine("");
+Console.WriteLine("Comments are importante beacause");
